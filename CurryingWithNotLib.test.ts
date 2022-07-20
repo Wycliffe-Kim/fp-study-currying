c4a40f0d600fcb63b4d969d48c8aeb8a261dc6e4
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-07-20 13:32:04 +0900

    currying with not lib

---
+++ CurryingWithNotLib.test.ts
@@ -0,0 +1,15 @@
+import { sub2, sum2 } from './curryingWithNotLib';
+
+test('3 + 3 = 6', () => {
+  const sumOne = sum2(1);
+  const sumTwo = sum2(2);
+  const sumThree = (value: number) => sumTwo(sumOne(value));
+  expect(sumThree(3)).toBe(6);
+});
+
+test('6 - 3 = 3', () => {
+  const subOne = sub2(1);
+  const subTwo = sub2(2);
+  const subThree = (value: number) => subTwo(subOne(value));
+  expect(subThree(6)).toBe(3);
+});
---
+++ curryingWithNotLib.ts
@@ -0,0 +1,2 @@
+export const sum2 = (value1: number) => (value2: number) => value2 + value1;
+export const sub2 = (value1: number) => (value2: number) => value2 - value1;
